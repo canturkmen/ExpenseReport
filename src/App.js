079d5624bc74bf95c9e7e2e83eabcc5e9852c9fa
canturkmen <turkmenc098@gmail.com>
2022-07-01 14:03:01 +0300

    Added our first component.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (<div>
+    <ExpenseItem/>
+  </div>);
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ExpenseItem() {
+    return (
+        <div>
+            <h2>Lets get started</h2>
+            <p>This is also visible</p>
+        </div>
+    );
+}; 
+
+export default ExpenseItem;
