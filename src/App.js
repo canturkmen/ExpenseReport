a445b12ab5fc73d7658e37a9d2c503ac057615e6
canturkmen <turkmenc098@gmail.com>
2022-07-02 22:12:45 +0300

    Changing few things.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./App.css";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 const expenses = [
   {
@@ -25,22 +25,13 @@ const expenses = [
   },
 ];
 
-function App() {
+const App = () => {
 
-  // This is our JSX syntax
-  // return (
-  //   <div>
-  //     <h2>Let's Get Started !</h2>
-  //     <Expenses expenses={expenses}></Expenses>;
-  //   </div>
-  // );
-
-  // This what the React does when you compile the code.
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started"),
-    React.createElement(Expenses, {expenses: expenses})
+  return (
+    <div>
+      <h2>Let's Get Started !</h2>
+      <Expenses expenses={expenses}></Expenses>;
+    </div>
   );
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import Card from "./Card";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
