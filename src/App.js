83816970ccb4de88a0823d25eeb78fe37d00f94a
canturkmen <turkmenc098@gmail.com>
2022-07-02 22:06:02 +0300

    Understanding more about JSX.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "./App.css";
 import Expenses from "./components/Expenses";
 
@@ -24,11 +26,21 @@ const expenses = [
 ];
 
 function App() {
-  return (
-    <div>
-      <h2>Let's Get Started !</h2>
-      <Expenses expenses={expenses}></Expenses>;
-    </div>
+
+  // This is our JSX syntax
+  // return (
+  //   <div>
+  //     <h2>Let's Get Started !</h2>
+  //     <Expenses expenses={expenses}></Expenses>;
+  //   </div>
+  // );
+
+  // This what the React does when you compile the code.
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Let's get started"),
+    React.createElement(Expenses, {expenses: expenses})
   );
 }
 
